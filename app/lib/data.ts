6e589c5e626b717a080e945c5d108b02afda4add
Serena Inzani <serenainzani@hotmail.com>
2024-08-30 13:56:57 +0100

    feat: chp 9 finished

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -33,12 +33,17 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    console.log('Data fetch completed after 2 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
